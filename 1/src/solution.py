1a83795cb8d4380cfc9d91a073a9eac29468382c
Устинова Дарья <vedmachka0063@gmail.com>
2026-01-04 20:13:27 +0300

    commit

--- 1/src/solution.py
+++ 1/src/solution.py
@@ -4,5 +4,21 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def add_movies(connection):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO movies (title, release_year, duration) VALUES (%s, %s, %s)",
+            ('Godfather', 1972, 175)
+        )
+        cursor.execute(
+            "INSERT INTO movies (title, release_year, duration) VALUES (%s, %s, %s)",
+            ('The Green Mile', 1999, 189)
+        )
+    connection.commit()
 
+
+def get_all_movies(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM movies ORDER BY id")
+        return cursor.fetchall()
 # END
--- 2/src/solution.py
+++ 2/src/solution.py
@@ -4,5 +4,37 @@ conn = psycopg2.connect('postgresql://postgres:@localhost:5432/test_db')
 
 
 # BEGIN (write your solution here)
+def make_cars_table(connection):
+    """Создает таблицу cars в базе данных"""
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS cars (
+                id SERIAL PRIMARY KEY,
+                brand VARCHAR(255) NOT NULL,
+                model VARCHAR(255) NOT NULL
+            )
+        """)
+    connection.commit()
 
+
+def populate_cars_table(connection, cars):
+    """Добавляет автомобили в таблицу cars"""
+    with connection.cursor() as cursor:
+        # Используем execute_batch для эффективной вставки нескольких записей
+        for car in cars:
+            cursor.execute(
+                "INSERT INTO cars (brand, model) VALUES (%s, %s)",
+                car
+            )
+    connection.commit()
+
+
+def get_all_cars(connection):
+    """Возвращает все автомобили из таблицы cars, отсортированные по марке"""
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT * FROM cars 
+            ORDER BY brand ASC
+        """)
+        return cursor.fetchall()
 # END
